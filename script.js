306cb5acd18a70ae1495fdbbb51dae88fbe66e20
Zachary Clark <zacclark369@gmail.com>
2022-08-17 17:04:07 -0700

    base questions and html format built, working on styling now

---
+++ script.js
@@ -0,0 +1,39 @@
+const quizData = [
+    {
+        question: 'What is the best form of transportation?',
+        a: 'Bike',
+        b: 'Car',
+        c: 'Scooter',
+        d: 'Bus',
+        correct: 'Bike'
+    },
+    {
+        question: 'What is the most used programming language in 2022?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'd'
+    }, {
+        question: 'Who is the president of the US?',
+        a: 'Ronaldo',
+        b: 'Biden',
+        c: 'Mohatma Ghandi',
+        d: 'Trump',
+        correct: 'b'
+    }, {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Helicopters Terminals Motorboats Lamborginis',
+        correct: 'a'
+    }, {
+        question: 'What year was JavaScript launched?',
+        a: '2020',
+        b: '1991',
+        c: '1985',
+        d: 'none of the above',
+        correct: 'd'
+    }
+]
